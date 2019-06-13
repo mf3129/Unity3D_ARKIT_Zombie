94d5707822c76a7bf67fdaee3a4dd7338d01c611
Mak Fof <43025563+mf3129@users.noreply.github.com>
2019-06-13 01:24:00 -0400

    created zombie script- properties and functions

---
+++ ZombieMovie.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMovie : MonoBehaviour
+{
+
+    public GameObject core;
+    public float speed; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        core = GameObject.FindGameObjectWithTag("Core"); //Assigns core object as a reference for spawning the zombie
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(core.transform); //Finding the core location
+        transform.Translate(Vector3.forward * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Arrow") //Checks to see if the object it was hit with is an arrow
+        {
+            Destroy(gameObject);
+        }
+    }
+}
