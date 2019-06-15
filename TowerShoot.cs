a6125070f63d3293de7b7af8055401f4e1a88029
Mak Fof <43025563+mf3129@users.noreply.github.com>
2019-06-15 14:40:39 -0400

    creation of the tower script & update tower prefab

---
+++ TowerShoot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerShoot : MonoBehaviour
+{
+    public Transform target;
+    public float shootForce;
+
+
+    public GameObject prefabArrow;  //Actually prefab for the Arrow
+    public GameObject arrow;  //Actual arrow in the scene
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.LookAt(target);
+            arrow = Instantiate(prefabArrow, transform.position, transform.rotation);
+            arrow.GetComponentRigidbody >.AddForce(transform.forward * shootForce);
+            Destroy(arrow, 2); //Destroying the arrow after 2 seconds
+
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+       if (other.tag == "Zombie" )
+        {
+            target = other.gameObject.transform;
+        }
+    }
+
+
+}
