016074a0aa7f0384c1e3a46cafa0534f00f3f507
Mak Fof <43025563+mf3129@users.noreply.github.com>
2019-06-15 17:11:36 -0400

    Update to tower script and delay in arrows.

--- TowerShoot.cs
+++ TowerShoot.cs
@@ -11,17 +11,21 @@ public class TowerShoot : MonoBehaviour
     public GameObject prefabArrow;  //Actually prefab for the Arrow
     public GameObject arrow;  //Actual arrow in the scene
 
+    public float delay;
+    public float countDown = 0; 
+
     // Update is called once per frame
     void Update()
     {
-        if (target != null)
+        if (target != null && countDown < 0)
         {
             transform.LookAt(target);
             arrow = Instantiate(prefabArrow, transform.position, transform.rotation);
-            arrow.GetComponentRigidbody >.AddForce(transform.forward * shootForce);
+            arrow.GetComponent<Rigidbody> ().AddForce(transform.forward * shootForce);
             Destroy(arrow, 2); //Destroying the arrow after 2 seconds
-
+            countDown = delay;
         }
+        countDown -= Time.deltaTime;
     }
 
     void OnTriggerStay(Collider other)
